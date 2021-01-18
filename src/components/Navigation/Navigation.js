49e164b817664964fbbb8b0320776f5dbeaeebf1
Alinea6 <natalia.grycz28@gmail.com>
2021-01-18 22:49:14 +0100

    Updating profile summary layout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,45 +2,45 @@ import React from 'react';
 
 const Navigation = () => {
     return (
-        <nav className='ba bw3 container'>
+        <nav className='ba bw2 container'>
             <div className='row pa0'>
-                <div className='col pa0 ba bg-near-black hover: dim'> 
+                <div className='col pa0 ba bg-black hover: dim'> 
                     <p className='tc f4 silver link pointer'>Lokacja</p>
                 </div>
-                <div className='col pa0 ba bg-near-black hover: dim'>
+                <div className='col pa0 ba bg-black hover: dim'>
                     <p className='tc f4 silver link pointer'>Wielka Sala</p>
                 </div>
-                <div className='col pa0 ba bg-near-black hover: dim'>
+                <div className='col pa0 ba bg-black hover: dim'>
                     <p className='tc f4 silver link pointer'>Pokój Wspólny</p>
                 </div>
-                <div className='col pa0 ba bg-near-black hover: dim'>
+                <div className='col pa0 ba bg-black hover: dim'>
                     <p className='tc f4 silver link pointer'>Lekcje</p>
                 </div>
             </div>
             <div className='row pa0'>
-                <div className='col pa0 ba bg-near-black hover: dim'>
+                <div className='col pa0 ba bg-black hover: dim'>
                     <p className='tc f4 silver link pointer'>Kufer</p>
                 </div>
-                <div className='col pa0 ba bg-near-black hover: dim'>
+                <div className='col pa0 ba bg-black hover: dim'>
                     <p className='tc f4 silver link pointer'>Poczta</p>
                 </div>
-                <div className='col pa0 ba bg-near-black hover: dim'>
+                <div className='col pa0 ba bg-black hover: dim'>
                     <p className='tc f4 silver link pointer'>Regulamin</p>
                 </div>
-                <div className='col pa0 ba bg-near-black hover: dim'>
+                <div className='col pa0 ba bg-black hover: dim'>
                     <p className='tc f4 silver link pointer'>Izba Pamięci</p>
                 </div>
             </div>
             <div className='row pa0'>
-                <div className='col pa0 ba bg-near-black hover: dim'>
+                <div className='col pa0 ba bg-black hover: dim'>
                     <p className='tc f4 silver link pointer'>Poradnik</p>
                 </div>
                     {/* {<p>Prasa</p>} - Prorok Codzienny, Żongler
                     <p> Pokój Nauczycielski</p> - Opcje dla rang */}
-                <div className='col pa0 ba bg-near-black hover: dim'>
+                <div className='col pa0 ba bg-black hover: dim'>
                     <p className='tc f4 silver link pointer'>Ustawienia</p>
                 </div>
-                <div className='col pa0 ba bg-near-black hover: dim'>
+                <div className='col pa0 ba bg-black hover: dim'>
                     <p className='tc f4 silver link pointer'>Wyloguj</p>
                 </div>
             </div>
--- src/components/ProfileSummary/ProfileSummary.js
+++ src/components/ProfileSummary/ProfileSummary.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import {ProgressBar} from 'react-bootstrap'
+
+let lifeNow = 100
+let actionPointsNow=100 
+let experienceNow=25
 
 const ProfileSummary = () => {
     return (
-        <div className='ba'>
-            <p>Login:</p>
-            <p>Expierience: </p>
+        <div className='ba bw2 container' style={{display: 'grid'}}>
+            <div className='row pa0'>
+                <div className='col pa0 ba bg-black'>
+                    <p className='tc f4 silver link pointer'>Login</p>
+                </div>
+            </div>
+            <div className='row pa0'>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <p className='pa0 f6 silver'>Punkty Życia: 10/10</p>
+                </div>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <p className='pa0 f6 silver'>Dom: nieprzydzielony</p>
+                </div>
+            </div>
+            <div className='row pa0'>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <ProgressBar striped variant="danger" now={lifeNow} />
+                </div>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <p className='pa0 f6 silver link pointer underline'>Karta postaci</p>
+                </div>
+            </div>
+            <div className='row pa0'>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <p className='pa0 f6 silver'>Punkty Akcji: 10/10</p>
+                </div>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <p className='pa0 f6 silver'>Pieniądze: 0g 0s 0k</p>
+                </div>
+            </div>
+            <div className='row pa0'>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <ProgressBar striped variant="success" now={actionPointsNow} />
+                </div>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <p className='pa0 f6 silver'>Skrytka: 0g 0s 0k</p>
+                </div>
+            </div>
+            <div className='row pa0'>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <p className='pa0 f6 silver'>Doświadczenie: 25/100</p>
+                </div>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <p className='pa0 f6 silver'>Poziom: 1</p>
+                </div>
+            </div>
+            <div className='row pa0'>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <ProgressBar striped variant="warning" now={experienceNow} />
+                </div>
+                <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 pa0 bg-black'>
+                    <p className='pa0 f6 silver'></p>
+                </div>
+            </div>
         </div>
     )
 }
